38e5929dfbb2490a8964e0b8f19dd43f1c82e07f
Eugene Konopaka <ikonopaka@binck.nl>
2020-06-01 11:47:02 +0200

    Created producer and consumer demo apps

--- demo-producer/src/main/java/nl/binck/kafka/demo/DemoProducer.java
+++ demo-producer/src/main/java/nl/binck/kafka/demo/DemoProducer.java
@@ -1,7 +1,9 @@
+package nl.binck.kafka.demo;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.Random;
@@ -16,8 +18,8 @@ public class DemoProducer {
     private static Properties getProducerProperties() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         return props;
     }
